80fd27f04cba1ed8b0164d7608396f9240b75155
Jonathan Austin <jonathan.austin@border.gov.au>
2018-02-09 14:34:43 +1100

    Refactor Modules

---
+++ wcomponents-addons-cardpath/src/main/java/com/github/bordertech/wcomponents/addons/cardpath/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Application security mechanism and URL driven card manager.
+ */
+package com.github.bordertech.wcomponents.addons.cardpath;
---
+++ wcomponents-addons-common/src/main/java/com/github/bordertech/wcomponents/addons/common/relative/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Allow a URL that is relative to the base URL to be used so it works in different application contexts without having
+ * to be changed.
+ */
+package com.github.bordertech.wcomponents.addons.common.relative;
